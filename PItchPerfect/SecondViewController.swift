8519e3ebd8331fd69907a4ffa1e01f90df026e5d
Luis Fernando <xvleg3ndaryxv@gmail.com>
2017-10-29 15:34:43 -0600

    Layout build with swift code

---
+++ PItchPerfect/SecondViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SecondViewController.swift
+//  PItchPerfect
+//
+//  Created by Luis Santiago  on 10/29/17.
+//  Copyright © 2017 Luis Santiago. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red;
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
--- PItchPerfect/ViewController.swift
+++ PItchPerfect/ViewController.swift
@@ -10,40 +10,75 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var RecordingLabel: UILabel!
-    @IBOutlet weak var StopRecording: UIButton!
-    @IBOutlet weak var RecordButton: UIButton!
-    @IBOutlet weak var StopRecordingLabel: UIButton!
+    let recordButton : UIButton = {
+        let button = UIButton();
+        button.translatesAutoresizingMaskIntoConstraints = false;
+        button.setImage(#imageLiteral(resourceName: "RecordButton"), for: .normal);
+        button.addTarget(self, action: #selector(onClickRecord), for: .touchUpInside);
+        return button;
+    }();
     
-    @IBAction func onClickRecord(_ sender: Any) {
-        RecordingLabel.text = "Recording in progess";
-        RecordButton.isEnabled = false;
-        StopRecordingLabel.isEnabled = true;
-    }
+    let textStatus : UITextView = {
+        let textView = UITextView();
+        textView.text = "Tap to record";
+        textView.translatesAutoresizingMaskIntoConstraints = false;
+        textView.isEditable = false;
+        textView.isScrollEnabled = false;
+        textView.textAlignment = .center;
+        textView.font = UIFont.systemFont(ofSize : 20);
+        return textView;
+    }();
     
-    @IBAction func onStop(_ sender: Any) {
-        RecordButton.isEnabled = true;
-        StopRecordingLabel.isEnabled = false;
-        print("We are printing");
-    }
+    let stopRecordButton : UIButton = {
+        let button = UIButton();
+        button.translatesAutoresizingMaskIntoConstraints = false;
+        button.setImage(#imageLiteral(resourceName: "Stop"), for: .normal);
+        button.addTarget(self, action: #selector(onStopClick), for: .touchUpInside);
+        button.isEnabled = false;
+        return button;
+    }();
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        StopRecordingLabel.isEnabled = false;
-        // Do nothing for now.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated);
-        // disable the recording button
-    
-
+        super.viewDidLoad();
+        navigationItem.title = "Pitch perfect";
+        view.backgroundColor = .white;
+        view.addSubview(recordButton);
+        view.addSubview(textStatus);
+        view.addSubview(stopRecordButton);
+        setUpLayout();
     }
     
-   
+    func setUpLayout(){
+        
+        //Layout for recording button
+        recordButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 100).isActive = true;
+        recordButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true;
+        recordButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true;
+        recordButton.heightAnchor.constraint(equalToConstant: 200).isActive = true;
+        
+        //Layout for textview
+        textStatus.topAnchor.constraint(equalTo: recordButton.bottomAnchor, constant : -15).isActive = true;
+        textStatus.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true;
+        textStatus.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true;
+        
+        //Stop button
+        stopRecordButton.topAnchor.constraint(equalTo: textStatus.bottomAnchor).isActive = true;
+        stopRecordButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true;
+        stopRecordButton.widthAnchor.constraint(equalToConstant: 80).isActive = true;
+        stopRecordButton.heightAnchor.constraint(equalToConstant: 80).isActive = true;
+    }
+    
+    @objc func onClickRecord(){
+        navigationController?.pushViewController(SecondViewController(), animated: true);
+        /*textStatus.text = "Recording";
+        recordButton.isEnabled = false;
+        stopRecordButton.isEnabled = true;*/
+    }
+    
+    @objc func onStopClick(){
+//        textStatus.text = "Click to record";
+//        recordButton.isEnabled = true;
+//        stopRecordButton.isEnabled = false;
+    }
 }
 
